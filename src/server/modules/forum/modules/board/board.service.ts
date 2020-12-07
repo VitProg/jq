a084922f0394175bbfeb41c53eeb717e5ba4a0af
Viktor Zhokhov <v.zhokhov@elama.ru>
2020-12-07 21:31:18 +0300

    client: improve cache system server: add board and category rest

--- src/server/modules/forum/modules/board/board.service.ts
+++ src/server/modules/forum/modules/board/board.service.ts
@@ -1,44 +1,53 @@
 import { Injectable } from '@nestjs/common'
-import { IBoard } from '../../../../../common/forum/forum.interfaces'
+import { IBoard, IUser } from '../../../../../common/forum/forum.interfaces'
 import { ForumCacheService } from '../forum-cache/forum-cache.service'
+import { getUserGroups } from '../../../../../common/forum/utils'
 
 
 @Injectable()
 export class BoardService {
   constructor (
-    private forumCacheService: ForumCacheService
+    private readonly cacheService: ForumCacheService
   ) {
   }
 
-  async findByIdsToMap(ids: number[] | Set<number>): Promise<Map<number, IBoard>> {
-    const idSet = new Set(ids)
-
-    const map = new Map<number, IBoard>()
-
-    const boardMap = await this.forumCacheService.getBoardMap()
-    const categoryMap = await this.forumCacheService.getCategoryMap()
-
-    for (const id of idSet) {
-      const board = boardMap.get(id)
-      if (board) {
-        map.set(id, {
-          ...board,
-          category: categoryMap.get(board.linksId.category)
-        })
-      }
-    }
-
-    return map;
+  async availableBoardIdsForUser (user?: IUser): Promise<number[]> {
+    const userGroups = getUserGroups(user)
+    const boards = await this.findAll(0, userGroups)
+    return boards.map(board => board.id)
   }
 
-  async findByIds(ids: number[] | Set<number>): Promise<IBoard[]> {
+  async findAll (parentId: number = 0, forGroups: number[] = [-1] /**todo**/): Promise<IBoard[]> {
+    const boardMap = await this.cacheService.getBoardMap()
+    const boardArray = [...boardMap.values()]
+    return boardArray
+      .filter(board => board.linksId.parent === parentId)
+      .filter(board => forGroups.length && board.forGroups ? forGroups.some(g => board.forGroups!.includes(g)) : true)
+  }
+
+
+  async findOne (id: string): Promise<IBoard | undefined> {
+    return (await this.cacheService.getBoardMap()).get(+id)
+  }
+
+
+  async findByIdsToMap (ids: number[] | Set<number>): Promise<Map<number, IBoard>> {
+    const idSet = new Set(ids)
+    const boardMap = await this.cacheService.getBoardMap()
+
+    return new Map<number, IBoard>(
+      [...boardMap.entries()]
+        .filter(([id]) => idSet.has(id))
+    )
+  }
+
+  async findByIds (ids: number[] | Set<number>): Promise<IBoard[]> {
     return [...(await this.findByIdsToMap(ids)).values()]
   }
 
-  async findByIdsToRecord(ids: number[] | Set<number>): Promise<Record<number, IBoard>> {
+  async findByIdsToRecord (ids: number[] | Set<number>): Promise<Record<number, IBoard>> {
     const map = await this.findByIdsToMap(ids)
     return Object.fromEntries(map.entries())
   }
-
 
 }
